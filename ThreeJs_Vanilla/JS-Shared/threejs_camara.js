b10f4a737d97f287f87f11aa2b937300f57e43c9
Andreius_14 <Andreius_14@outlook.com>
2025-04-23 02:11:53 -0500

    Movimiento

--- ThreeJs_Vanilla/JS-Shared/threejs_camara.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from "three";
-
-export function newCamara() {
-  const camara = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500,);
-  return camara;
-}
-
-// Producto individual no necesita de un Tercero
